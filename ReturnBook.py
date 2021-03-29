3d650c0c875ed2aa6e710a91c34ffc22a426dc73
chisoftltd <chisoftltd@gmail.com>
2021-03-30 00:50:17 +0200

    Python pyodbc connect to Sql Server using SQL Server Authentication

--- ReturnBook.py
+++ ReturnBook.py
@@ -9,7 +9,7 @@ server = 'DESKTOP-EGLSDIK'
 database = 'LibraryManagementSystem' 
 # username = 'myusername' 
 # password = 'mypassword' 
-cnxn = pyodbc.connect(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=server;DATABASE=database;Trusted_Connection=yes;')
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Trusted_Connection=yes;')
 cursor = cnxn.cursor()
 
 # Enter Table Names here
